7a5a8077d844ceeae9fde184b384358f5694d2a4
mcfwesh <mcfwesh@gmail.com>
2023-07-16 16:42:35 +0200

    modify icon sizes;

--- app/components/AnimatePages.tsx
+++ app/components/AnimatePages.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { AnimatePresence } from "framer-motion";
-import React from "react";
+import React, { Fragment } from "react";
 import { usePathname } from "next/navigation";
 
 const AnimatePages = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   return (
     <AnimatePresence initial={false} mode="wait" key={pathname}>
-      <div key={pathname}>{children}</div>
+      <Fragment key={pathname}>{children}</Fragment>
     </AnimatePresence>
   );
 };
