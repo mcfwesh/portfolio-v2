8f31b26b9d202e8053946a8975ee6d993980bb41
mcfwesh <mcfwesh@gmail.com>
2023-07-29 11:56:07 +0200

    add link to expense app

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -354,7 +354,7 @@ const config = {
             url: "https://github.com/mcfwesh/project2-expenseapp",
             logo: GithubIcon,
           },
-          demo: "https://github.com/mcfwesh/project2-expenseapp",
+          demo: "https://expense-app-lm8h.onrender.com",
           technologies: [
             { name: "AWS", logo: HandlebarsIcon },
             { name: "AWS", logo: JavascriptIcon },
