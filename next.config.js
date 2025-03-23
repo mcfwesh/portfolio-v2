7da2d7454cfcf537f4774ac6082661da95542c08
mcfwesh <mcfwesh@gmail.com>
2025-03-23 13:18:38 -0400

    add remotePatterns configuration for images in next.config.js to allow loading images from GitHub

--- next.config.js
+++ next.config.js
@@ -25,6 +25,16 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
