e6a002777324ed8a1b4d4a0773cb80e03c1f6fbd
mcfwesh <mcfwesh@gmail.com>
2025-03-23 19:44:15 -0400

    update GoogleAnalytics component to set script loading strategy to 'afterInteractive' for improved performance

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -11,9 +11,10 @@ const GoogleAnalytics = ({
   return (
     <>
       <Script
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       />
-      <Script id="google-analytics">
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
         window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
